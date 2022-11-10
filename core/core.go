ab571bb532b062092e469061ab55f0b784008ecb
Jan Rehanek <rehanekjan@gmail.com>
2022-11-10 16:37:10 +0100

    Commented core code and expanded Entities module

--- core/core.go
+++ core/core.go
@@ -1,3 +1,4 @@
+// Package core contains the essential components for connecting GoodData Cloud's API.
 package core
 
 import (
@@ -5,6 +6,7 @@ import (
 	"net/http"
 )
 
+// ClientGD is needed for subsequent API calls from the entities module.
 type ClientGD struct {
 	Client      http.Client
 	BaseURL     string
@@ -14,6 +16,8 @@ type ClientGD struct {
 	Body        io.Reader
 }
 
+// SendRequest dispatches an API request according to specs in ClientGD. It returns *http.Response and a nil error if problem-free.
+// All headers listed in ClientGD are added to the request. Authorization header with the Bearer token is mandatory for success.
 func (cgd ClientGD) SendRequest() (*http.Response, error) {
 	req, err := http.NewRequest(cgd.Method, cgd.BaseURL+cgd.EndpointURL, cgd.Body)
 	if err != nil {
@@ -32,18 +36,21 @@ func (cgd ClientGD) SendRequest() (*http.Response, error) {
 	return resp, nil
 }
 
+// Creates and returns the basic ClientGD struct populated with the Authorization header and the baseUrl onto which API endpoints will be added.
+// The token needs to be a valid GD Cloud bearer token. baseURL should be the full hostname of your domain. Example: https://testdomain.cloud.gooddata.com
 func CreateClient(token string, baseUrl string) ClientGD {
 	client := http.Client{}
 	headers := map[string]string{
 		"Authorization": "Bearer " + token,
+		"Accept":        "application/vnd.gooddata.api+json",
 	}
 
-	clientGD := ClientGD{
+	cgd := ClientGD{
 		Client:  client,
 		BaseURL: baseUrl,
 		Headers: headers,
 		Method:  "GET",
 	}
 
-	return clientGD
+	return cgd
 }
--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/jan-r-dev/go_sdk_hackathon_2022
-
-go 1.19
